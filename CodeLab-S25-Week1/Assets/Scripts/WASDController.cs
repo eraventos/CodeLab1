27456686a16a9054087c298c8ffcb265c3306885
eraventos <er3440@nyu.edu>
2025-01-28 10:03:16 -0500

    Movement & Maze
    Use Arrow Keys to move up, down, left and right. Maze to move cube around

--- CodeLab-S25-Week1/Assets/Scripts/WASDController.cs
+++ CodeLab-S25-Week1/Assets/Scripts/WASDController.cs
@@ -3,19 +3,19 @@ using UnityEngine;
 public class WASDController : MonoBehaviour
 {
     public KeyCode keyUp = KeyCode.W;
-
+    public KeyCode keyDown = KeyCode.S;
+    public KeyCode keyLeft = KeyCode.A;
+    public KeyCode keyRight = KeyCode.D;
     public Rigidbody rb;
 
     public float moveForce = 10f;
-
-    Vector3 testVec;
+    
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Debug.Log("START!!!");
-
-        rb = null;
+        
     }
 
     // Update is called once per frame
@@ -25,7 +25,22 @@ public class WASDController : MonoBehaviour
 
         if (Input.GetKey(keyUp))
         {
-            rb.AddForce(testVec * moveForce);
+            rb.AddForce(Vector3.up * moveForce);
+        }
+        
+        if (Input.GetKey(keyDown))
+        {
+            rb.AddForce(Vector3.down * moveForce);
+        }
+        
+        if (Input.GetKey(keyLeft))
+        {
+            rb.AddForce(Vector3.left * moveForce);
+        }
+        
+        if (Input.GetKey(keyRight))
+        {
+            rb.AddForce(Vector3.right * moveForce);
         }
     }
 }
